514b4b824582afd9bcc10dbc34f901b7e9277b37
Egor Kukanov <balovenisydibbl@gmail.com>
2023-08-29 18:33:58 +0300

    docs: added tsconfig

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '2fa64e2d73e64b1081d4f03a76dd922a',
         });
     }
 }
