fce55e0689a32f61ef3ea63460fd08a6a49cb03b
Egor Kukanov <balovenisydibbl@gmail.com>
2023-09-11 16:48:10 +0300

    feat: sources.js in ts

---
+++ src/components/view/sources/sources.ts
@@ -0,0 +1,24 @@
+import './sources.css';
+interface SourceItem {
+    name: string;
+    id: string;
+}
+class Sources {
+    draw(data: SourceItem[]): void {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item): void => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+
+            (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        (document.querySelector('.sources') as HTMLElement).append(fragment);
+    }
+}
+
+export default Sources;
